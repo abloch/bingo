9a63acfad371d5836918718ad2503f203797a1fd
Akiva <akiva@simplex.com>
2018-06-27 09:36:48 +0300

    bla

--- bingo.py
+++ bingo.py
@@ -1,8 +1,8 @@
 from random import sample
 from os import listdir
 
-image_suffixes = ['.jpg', '.jpeg', '.png']
-pics = [filename for filename in listdir() if any(filename.lower().endswith(suffix) for suffix in image_suffixes)]
+image_suffixes = ['.jpg']
+pics = [filename for filename in listdir('.') if any(filename.lower().endswith(suffix) for suffix in image_suffixes)]
 
 def get_sample(num):
     return sample(pics, num)
@@ -13,17 +13,17 @@ def chunks(l, n):
         yield l[i:i + n]
 
 def imagize(src):
-    return '<img src="../{}" width="400px">'.format(src)
+    return '<img src="./{}">'.format(src)
 
 def generate_3_row(chunks):
     (a,b,c)=chunks
-    return "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(imagize(a), imagize(b), imagize(c))
+    return "<tr><td align='center'>{}</td><td align='center'>{}</td><td align='center'>{}</td></tr>".format(imagize(a), imagize(b), imagize(c))
 
 def generate_bingo(sample):
     chunks3 = list(chunks(sample, 3))
     rows = [generate_3_row(chunk) for chunk in chunks3]
     table = "".join(rows)
-    ret = "<html><head><style>.td img {{display: block;}}</style></head><body><table><tbody>{}</tbody></table></body></html>".format(table)
+    ret = "<html><head><style>.td img {{display: block !important;}}</style></head><body><table><tbody>{}</tbody></table></body></html>".format(table)
     return ret
 
 def generate_file(num):
